66b7d94521db14ece5330eeba588d8c95faec944
Noah Day <sheltielover432@gmail.com>
2020-04-20 14:53:00 -0400

    Making the Enemy Chase and Lose the Player

--- Udemy FPS/Assets/Scripts/EnemyController.cs
+++ Udemy FPS/Assets/Scripts/EnemyController.cs
@@ -6,6 +6,9 @@ public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody theRB;
+    private bool chasing;
+    public float distanceToChase = 10f, distanceToLose = 15f;
+    private Vector3 targetPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,23 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(PlayerController.instance.transform.position);
-        theRB.velocity = transform.forward * moveSpeed;
+        targetPoint = PlayerController.instance.transform.position;
+        targetPoint.y = transform.position.y;
+        if(!chasing)
+        {
+            if(Vector3.Distance(transform.position, targetPoint)<distanceToChase)
+            {
+                chasing = true;
+            }
+        }
+        else
+        {
+            transform.LookAt(targetPoint);
+            theRB.velocity = transform.forward * moveSpeed;
+            if(Vector3.Distance(transform.position, targetPoint)>distanceToLose)
+            {
+                chasing = false;
+            }
+        }
     }
 }
