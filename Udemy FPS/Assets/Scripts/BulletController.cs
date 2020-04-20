ad7c7ebdf2ec21a80e3b0db48d32b7a735678dd1
Noah Day <sheltielover432@gmail.com>
2020-04-20 12:28:56 -0400

    Destroying Targets

--- Udemy FPS/Assets/Scripts/BulletController.cs
+++ Udemy FPS/Assets/Scripts/BulletController.cs
@@ -26,7 +26,11 @@ public class BulletController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
         Destroy(gameObject);
-        Instantiate(impactEffect, transform.position, transform.rotation);
+        Instantiate(impactEffect, transform.position +(transform.forward * (-moveSpeed * Time.deltaTime)), transform.rotation);
     }
 }
