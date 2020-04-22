5b7256e9a746a4363f8ecba90505bb81e4aaab15
Noah Day <sheltielover432@gmail.com>
2020-04-22 13:56:53 -0400

    Fading Between Levels

--- Udemy FPS/Assets/Scripts/UIController.cs
+++ Udemy FPS/Assets/Scripts/UIController.cs
@@ -8,8 +8,8 @@ public class UIController : MonoBehaviour
     public static UIController instance;
     public Slider healthSlider;
     public Text healthText, ammoText;
-    public Image damageEffect;
-    public float damageAlpha = 0.25f, damageFadeSpeed=2f;
+    public Image damageEffect, blackScreen;
+    public float damageAlpha = 0.25f, damageFadeSpeed=2f, fadeSpeed = 1.5f;
     public GameObject pauseScreen;
     private void Awake()
     {
@@ -27,6 +27,14 @@ public class UIController : MonoBehaviour
         if(damageEffect.color.a !=0)
         {
             damageEffect.color = new Color(damageEffect.color.r, damageEffect.color.g, damageEffect.color.b, Mathf.MoveTowards(damageEffect.color.a, 0f, damageFadeSpeed * Time.deltaTime));
+        }
+        if(!GameManager.instance.levelEnding)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+        }
+        else
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
         }
     }
     public void ShowDamage()
